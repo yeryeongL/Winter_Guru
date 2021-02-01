3e0195f15aa33f15030822f1d0b6cc15a01920fa
이예령 <yeryeong@iyelyeong-ui-MacBookPro.local>
2021-02-01 14:27:12 +0900

    create collectionview

--- FoodRecommend/FoodRecommend/ViewController.swift
+++ FoodRecommend/FoodRecommend/ViewController.swift
@@ -12,8 +12,29 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
+            }
 
 
 }
+
+extension ViewController:UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "foodCell", for: indexPath)
+        return cell
+    }
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout{
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = self.view.frame.size.width / 3-5
+        let height = width * 1.5
+        
+        return CGSize(width: width, height: height)
+    }
+}
+
 
